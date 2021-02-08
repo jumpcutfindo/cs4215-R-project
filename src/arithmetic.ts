2e2e4df1b20ef8409a9b06f7e833f98a3b23df16
Daniel Hoe <danielhoe97@gmail.com>
2021-02-08 16:25:42 +0800

    More updates to arithmetic operations
    Changed types from 'any' to 'Logical | Int | Real', added warnings and error printing

--- src/arithmetic.ts
+++ src/arithmetic.ts
@@ -1,4 +1,4 @@
-import {Logical, Int, Real, RNull} from './types';
+import {Logical, Int, Real, RNull, RValue, Character} from './types';
 
 const type_hierarchy = ['logical', 'integer', 'numeric'];
 
@@ -14,52 +14,72 @@ const arithmetic_functions: any = {
 
 function apply_arithmetic_operation(
     operator: any,
-    first_operand: any,
-    second_operand: any,
+    first_operand: RValue,
+    second_operand: RValue,
 ) {
-    let operands = {first_operand, second_operand};
+    if (
+        type_hierarchy.indexOf(first_operand.tag) == -1 ||
+        type_hierarchy.indexOf(second_operand.tag) == -1
+    ) {
+        // Error: type doesn't exist in our hierarchy
+        console.error(`Error: non-numeric argument to binary operator`);
+        return;
+    }
+
+    let operands = {
+        first_operand: first_operand as Logical | Int | Real,
+        second_operand: second_operand as Logical | Int | Real,
+    };
+
     // 1. Coercion to the same type
     if (operands.first_operand.tag !== operands.second_operand.tag) {
-        try {
-            operands = coerce_types(first_operand, second_operand);
-        } catch {
-            // Error handling on failure to coerce types
-        }
+        operands = coerce_types(
+            first_operand as Logical | Int | Real,
+            second_operand as Logical | Int | Real,
+        );
     }
 
-    const arithmetic_result_type = operands.first_operand.tag;
+    let arithmetic_result_type = operands.first_operand.tag;
+    // 2. Check for the expected type depending on the arithmetic operator
+    if (operator === '%/%') {
+        arithmetic_result_type = 'integer';
+    }
 
-    // 2. If both are logical types, convert to integer types
+    // 3. If both are logical types, convert to integer types
     if (arithmetic_result_type == 'logical') {
         operands.first_operand =
-            coerce_operand_to_type(first_operand, 'integer');
+            coerce_operand_to_type(
+                first_operand as Logical | Int | Real,
+                'integer',
+            );
         operands.second_operand =
-            coerce_operand_to_type(second_operand, 'integer');
+            coerce_operand_to_type(second_operand as Logical | Int | Real,
+                'integer',
+            );
     }
 
-    // 3. Check vector lengths and do recycling
+    // 4. Check vector lengths and do recycling
     if (operands.first_operand.data.length !==
         operands.second_operand.data.length) {
-        try {
-            operands = recycle(operands.first_operand, operands.second_operand);
-        } catch {
-            // Error handling on failure to recycle operands
-        }
+        operands = recycle(operands.first_operand, operands.second_operand);
     }
 
-    // 4. Carry out operation
+    // 5. Carry out operation
     const arithmetic_result = arithmetic_functions[operator](
         operands.first_operand.data,
         operands.second_operand.data,
     );
 
-    // 5. Return result as a newly created vector
+    // 6. Return result as a newly created vector
     return create_vector_of_type(arithmetic_result, arithmetic_result_type);
 }
 
-function recycle(first_operand: any, second_operand: any) {
-    let shorter_operand;
-    let longer_operand;
+function recycle(
+    first_operand: Logical | Int | Real,
+    second_operand: Logical | Int | Real,
+) {
+    let shorter_operand: Logical | Int | Real;
+    let longer_operand: Logical | Int | Real;
 
     if (first_operand.data.length > second_operand.data.length) {
         longer_operand = first_operand;
@@ -70,17 +90,19 @@ function recycle(first_operand: any, second_operand: any) {
     }
 
     if (longer_operand.data.length % shorter_operand.data.length != 0) {
-        // Throw error: Length of shorter vector not a multiple of longer vector
-    } else {
-        const recycled_data = [];
-        const factor = shorter_operand.data.length;
-
-        for (let i = 0; i < longer_operand.data.length; i ++) {
-            recycled_data.push(shorter_operand.data[i % factor]);
-        }
-
-        shorter_operand.data = recycled_data;
+        console.warn(
+            // eslint-disable-next-line max-len
+            'Warning: longer object length is not a multiple of shorter object length',
+        );
     }
+
+    const factor: number = shorter_operand.data.length;
+
+    shorter_operand.data = (longer_operand.data as any).map(
+        (x: any, index: number) => {
+            return shorter_operand.data[index % factor];
+        },
+    );
 
     return {
         first_operand: longer_operand,
@@ -88,93 +110,74 @@ function recycle(first_operand: any, second_operand: any) {
     };
 }
 
-function coerce_types(first_operand: any, second_operand: any) {
+function coerce_types(
+    first_operand: Logical | Int | Real,
+    second_operand: Logical | Int | Real,
+) {
     let first_operand_modified;
     let second_operand_modified;
 
-    if (
-        type_hierarchy.indexOf(first_operand.tag) == -1 ||
-        type_hierarchy.indexOf(second_operand.tag) == -1
-    ) {
-        // Throw error: type doesn't exist in our hierarchy
-    } else {
-        const type_index = Math.max(
-            type_hierarchy.indexOf(first_operand.tag),
-            type_hierarchy.indexOf(second_operand.tag),
+    const type_index = Math.max(
+        type_hierarchy.indexOf(first_operand.tag),
+        type_hierarchy.indexOf(second_operand.tag),
+    );
+    switch (type_hierarchy[type_index]) {
+    case 'logical':
+        first_operand_modified = coerce_operand_to_type(
+            first_operand,
+            'logical',
         );
-        console.log('type hierarchy', type_hierarchy[type_index]);
-        switch (type_hierarchy[type_index]) {
-        case 'logical':
-            first_operand_modified = coerce_operand_to_type(
-                first_operand,
-                'logical',
-            );
 
-            second_operand_modified = coerce_operand_to_type(
-                second_operand,
-                'logical',
-            );
-            break;
-        case 'integer':
-            first_operand_modified = coerce_operand_to_type(
-                first_operand,
-                'integer',
-            );
+        second_operand_modified = coerce_operand_to_type(
+            second_operand,
+            'logical',
+        );
+        break;
+    case 'integer':
+        first_operand_modified = coerce_operand_to_type(
+            first_operand,
+            'integer',
+        );
 
-            second_operand_modified = coerce_operand_to_type(
-                second_operand,
-                'integer',
-            );
-            break;
-        case 'numeric':
-            first_operand_modified = coerce_operand_to_type(
-                first_operand,
-                'numeric',
-            );
+        second_operand_modified = coerce_operand_to_type(
+            second_operand,
+            'integer',
+        );
+        break;
+    default:
+        first_operand_modified = coerce_operand_to_type(
+            first_operand,
+            'numeric',
+        );
 
-            second_operand_modified = coerce_operand_to_type(
-                second_operand,
-                'numeric',
-            );
-            break;
-        }
+        second_operand_modified = coerce_operand_to_type(
+            second_operand,
+            'numeric',
+        );
+        break;
     }
 
-    if (first_operand_modified && second_operand_modified &&
-        (first_operand_modified.tag === second_operand_modified.tag)) {
-        return {
-            first_operand: first_operand_modified,
-            second_operand: second_operand_modified,
-        };
-    } else {
-        return {
-            first_operand: undefined,
-            second_operand: undefined,
-        };
-
-        // Throw error: Types not equal even after coercion
-    }
+    return {
+        first_operand: first_operand_modified,
+        second_operand: second_operand_modified,
+    };
 }
 
-function coerce_operand_to_type(operand: any, type: any) {
-    if (type_hierarchy.indexOf(type) == -1) {
-        // Throw error: type doesn't exist in our hierarchy
-    }
-
+function coerce_operand_to_type(operand: Logical | Int | Real, type: string) {
     switch (type) {
     case 'logical':
         return {
             attributes: operand.attributes,
             refcount: operand.refcount,
             tag: 'logical',
-            data: operand.data.map((x: any) => x == 0 ? false : true),
+            data: (operand.data as any).map((x: any) => x == 0 ? false : true),
         } as Logical;
     case 'integer':
         return {
             attributes: operand.attributes,
             refcount: operand.refcount,
             tag: 'integer',
-            data: operand.data.map((x: any) => {
+            data: (operand.data as any).map((x: any) => {
                 if (operand.tag == 'logical') {
                     return x ? 1 : 0;
                 } else {
@@ -182,12 +185,12 @@ function coerce_operand_to_type(operand: any, type: any) {
                 }
             }),
         } as Int;
-    case 'numeric':
+    default:
         return {
             attributes: operand.attributes,
             refcount: operand.refcount,
             tag: 'numeric',
-            data: operand.data.map((x: any) => {
+            data: (operand.data as any).map((x: any) => {
                 if (operand.tag == 'logical') {
                     return x ? 1 : 0;
                 } else {
@@ -198,7 +201,7 @@ function coerce_operand_to_type(operand: any, type: any) {
     }
 }
 
-function create_vector_of_type(data: any, type: any) {
+function create_vector_of_type(data: [boolean | number], type: string) {
     switch (type) {
     case 'logical':
         return {
@@ -214,13 +217,13 @@ function create_vector_of_type(data: any, type: any) {
             tag: 'integer',
             data: data,
         } as Int;
-    case 'numeric':
+    default:
         return {
             attributes: RNull,
             refcount: 0,
             tag: 'numeric',
             data: data,
-        } as Real
+        } as Real;
     }
 }
 
@@ -271,7 +274,8 @@ function power(
 ) {
     return first_operand_data.map((num, index) => {
         const other_num = second_operand_data[index];
-        return (num !== null && other_num !== null) ? Math.pow(num, other_num) : null;
+        return (num !== null && other_num !== null) ?
+            Math.pow(num, other_num) : null;
     });
 }
 
@@ -291,8 +295,7 @@ function integer_division(
 ) {
     return first_operand_data.map((num, index) => {
         const other_num = second_operand_data[index];
-        return (num !== null && other_num !== null) ? Math.floor(num / other_num) : null;
+        return (num !== null && other_num !== null) ?
+            Math.floor(num / other_num) : null;
     });
 }
-
-
